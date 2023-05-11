d07187f0fdcb413db6c266c0f960f2b29c03e210
ndongMichel <biyoondong1996@gmail.com>
2023-05-11 23:25:13 +0100

    new model

--- page/views.py
+++ page/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
-class HomePageView(TemplateView):
+
+class HomePageView(ListView):
+    model = Post
+    context_object_name = 'all_post_list'
     template_name = 'home.html' 
 
 class AboutPageView(TemplateView):
