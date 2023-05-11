2f6d39bd69276141da1f3f866c099cebb930d84e
ndongMichel <biyoondong1996@gmail.com>
2023-05-12 00:26:12 +0100

    Regression detected from there, so this is what it looks like huu

--- page/urls.py
+++ page/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, DetailPostPage
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('detail/<int:pk>/', DetailPostPage.as_view(), name='detail'),
 ]
--- page/views.py
+++ page/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -8,6 +8,11 @@ class HomePageView(ListView):
     model = Post
     context_object_name = 'all_post_list'
     template_name = 'home.html' 
+
+class DetailPostPage(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
