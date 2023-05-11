259de4ddaf63cd19f333218afbd779f53b094c37
ndongMichel <biyoondong1996@gmail.com>
2023-05-11 23:41:51 +0100

    new test for the model and the pages

--- page/tests.py
+++ page/tests.py
@@ -1,10 +1,29 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
-class SimpleTests(SimpleTestCase):
-    def test_home_page_works(self):
-        response = self.client.get('/')
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="Oubliez Arafat, le nouveau roi est dans la place")
+    
+    def test_view_url_exists_at_proper_location(self):
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
     
-    def test_about_page_works(self):
-        response = self.client.get('/about/')
+    def test_url_by_name(self):
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text = 'Chérie coco, c\'est la magie !')
+    
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f"{post.text}"
+        self.assertEqual(expected_object_name, 'Chérie coco, c\'est la magie !')
+        
